f1175934b9aa292f6dd3ba4e2a274efef58d465b
maurasqq <maurasphqq@gmail.com>
2026-09-07 19:11:34 -0600

    Initializer

---
+++ src/main/java/ni/edu/uam/api_empleados/ApiEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.api_empleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmpleadosApplication.class, args);
+	}
+
+}
